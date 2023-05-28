38acfab7539431eb326e534bc351079b7f43d377
Hayato <m8810.gt.0119@gmail.com>
2023-05-28 21:16:29 +0900

    20230528 commit3

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('top/', views.top_page_view, name="top"),
+    path('', views.top_page_view, name="top"),
     path('project/<int:pk>/', views.project_page_view, name="project"),
     path('contact/', views.contact_form_view, name="contact"),
 ]
