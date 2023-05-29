2e5c02af3ccacac25cf1e0285a19e0636a0c0bb4
Hayato <m8810.gt.0119@gmail.com>
2023-05-29 16:41:35 +0900

    20230529 commit

--- projectapp/views.py
+++ projectapp/views.py
@@ -4,43 +4,59 @@ from django.core.mail import send_mail
 from .forms import ContactForm
 
 # Create your views here.
+
 def top_page_view(request):
+    # トップページを表示するビュー関数
     template_name = "top_page.html"
+    
+    # ポートフォリオアイテムの一覧を取得
     items = models.Portfolio_Item.objects.all()
-    return render(request, template_name, {'items':items})
+    
+    return render(request, template_name, {'items': items})
 
 def contact_form_view(request):
+    # お問い合わせフォームを表示するビュー関数
     template_name = "contact_form.html"
     
     if request.method == 'POST':
+        # POSTリクエストの場合、フォームの入力を処理する
         form = ContactForm(request.POST)
+        
         if form.is_valid():
             # フォームの入力が有効な場合の処理
             name = form.cleaned_data['name']
             company = form.cleaned_data['company']
             email = form.cleaned_data['email']
             message = form.cleaned_data['message']
-
-            # データベースに保存などの処理を行うことができます
+            
+            # 入力データをデータベースに保存
             contact_message = models.ContactMessage(name=name, company=company, email=email, message=message)
             contact_message.save()
-
-            send_mail(
-                'New contact message: '+ contact_message.name,
-                'company:\n' + contact_message.company + "\n\n" +
-                'email-address:\n' + contact_message.email + "\n\n" +
-                'message:\n' + contact_message.message,
-                'm8810.work.0119@gmail.com',  # 送信元メールアドレス
-                ['m8810.gt.0119@gmail.com'],  # 管理者のメールアドレス
-                fail_silently=False,
-            )
+            
+            # 管理者に新しいお問い合わせメッセージを送信
+            # send_mail(
+            #     'New contact message: ' + contact_message.name,
+            #     'company:\n' + contact_message.company + "\n\n" +
+            #     'email-address:\n' + contact_message.email + "\n\n" +
+            #     'message:\n' + contact_message.message,
+            #     'm8810.work.0119@gmail.com',  # 送信元メールアドレス
+            #     ['m8810.gt.0119@gmail.com'],  # 管理者のメールアドレス
+            #     fail_silently=False,
+            # )
+            
         return render(request, 'contact_success.html', {'form': form, 'success': True})
+    
     else:
+        # GETリクエストの場合、フォームを初期化して表示
         form = ContactForm()
+    
+    return render(request, template_name, {"form": form, 'success': False})
 
-    return render(request, template_name, {"form":form, 'success': False})
-
-def project_page_view(request,pk):
+def project_page_view(request, pk):
+    # プロジェクト詳細ページを表示するビュー関数
     template_name = "project_page.html"
+    
+    # 指定されたIDに対応するポートフォリオアイテムを取得
     item = models.Portfolio_Item.objects.get(pk=pk)
-    return render(request, template_name, {'item':item})
+    
+    return render(request, template_name, {'item': item})
--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,78 @@
-$(function(){
-    // $(window).on('load',function(){
-    //     $("#loading").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-    //     $("#loading p").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
-    //   });
+function TextTypingAnime() {
+    // 'loading'要素内の各 'p' 要素に対して処理を行う
+    $('#loading p').each(function() {
+        var elemPos = $(this).offset().top - 50; // 要素の位置を取得
+        var scroll = $(window).scrollTop(); // スクロール位置を取得
+        var windowHeight = $(window).height(); // ウィンドウの高さを取得
+        var thisChild = "";
+
+        if (scroll >= elemPos - windowHeight) {
+            // 要素が画面内に表示されている場合
+            thisChild = $(this).children();
+            thisChild.each(function(i) {
+                var time = 150; // アニメーションの遅延時間
+                $(this).delay(time * i).fadeIn(time); // 子要素を順番にフェードイン
+            });
+        } else {
+            // 要素が画面外にある場合
+            thisChild = $(this).children();
+            thisChild.each(function() {
+                $(this).stop();
+                $(this).css("display", "none"); // 子要素を非表示にする
+            });
+        }
+    });
+}
+
+$(function() {
+    var ref = document.referrer; // リファラ情報を得る
+    var hereHost = window.location.hostname; // 現在ページのホスト(ドメイン)名を得る
+
+    var sStr = "^https?://" + hereHost; // ホスト名が含まれるか探す正規表現を作成
+    var rExp = new RegExp(sStr, "i");
+
+    var sessionKey = 'firstVisitFlag'; // セッションストレージに保存しているフラグのキー
+
+    if (!ref.match(rExp) && !sessionStorage.getItem(sessionKey)) {
+        // アクセス元がサイト外かつセッションストレージにフラグがない場合の処理
+        $(window).on('load', function() {
+            var element = $("#loading p");
+            element.each(function() {
+                var text = $(this).html();
+                var textbox = "";
+                text.split('').forEach(function(t) {
+                    if (t !== " ") {
+                        textbox += '<span>' + t + '</span>';
+                        $(element).css("display", "block");
+                    } else {
+                        textbox += t;
+                    }
+                });
+                $(this).html(textbox);
+            });
+
+            TextTypingAnime(); // タイピングアニメーションの実行
+
+            $("#loading").delay(4000).fadeOut('slow'); // ローディング要素をフェードアウト
+            $("#loading p").delay(4000).fadeOut('slow'); // ローディング要素内の 'p' 要素をフェードアウト
+
+            // セッションストレージにフラグをセット
+            sessionStorage.setItem(sessionKey, true);
+        });
+    } else {
+        // セッションストレージにフラグがある場合は非表示にする
+        $("#loading").css("display", "none");
+        $("#loading p").css("display", "none");
+    }
 
     var portfolio_length = $('.portfolio-item').length;
     var portfolio_H = $('.portfolio-item').outerHeight();
-    $('#projects').css("height",portfolio_H*portfolio_length+146+"px");
+    $('#projects').css("height", portfolio_H * portfolio_length + 146 + "px");
 
-    $('#page-top a').click(function () {
+    $('#page-top a').click(function() {
         $('body,html').animate({
-            scrollTop: 0//ページトップまでスクロール
-        });//ページトップスクロールの速さ。数字が大きいほど遅くなる
-        return false;//リンク自体の無効化
+            scrollTop: 0 // ページトップまでスクロール
+        }); // ページトップスクロールの速さ。数字が大きいほど遅くなる
+        return false; // リンク自体の無効化
     });
-
 });
